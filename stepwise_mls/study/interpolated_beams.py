a4eb2f842a8fb7b4550c7b6a796187b1cd407856
micha <michaelkovaliov97@gmail.com>
2025-07-02 12:01:49 +0300

    Adjustments

--- stepwise_mls/study/interpolated_beams.py
+++ stepwise_mls/study/interpolated_beams.py
@@ -3,7 +3,6 @@ from dataclasses import dataclass
 import numpy as np
 
 import finufft
-
 from matplotlib import pyplot as plt
 from numba import njit
 
@@ -16,6 +15,7 @@ class Setting:
     base_points: str
     dropout: float
     delta: float
+
 
 @njit
 def point_distance(p1, p2):
@@ -30,8 +30,9 @@ if __name__ == '__main__':
 
     settings = []
     for base_points in ['halton']:
-        settings.append(Setting(base_points=base_points, dropout=0.25, delta=0.1))
-        settings.append(Setting(base_points=base_points, dropout=0.25, delta=0.05))
+        settings.append(Setting(base_points=base_points, dropout=0.1, delta=0.1))
+        settings.append(Setting(base_points=base_points, dropout=0.1, delta=0.01))
+        settings.append(Setting(base_points=base_points, dropout=0.1, delta=0.001))
 
     for setting in settings:
         lines = []
@@ -40,7 +41,7 @@ if __name__ == '__main__':
             point_a = (0, intercept)
             point_b = (np.pi, np.pi * slope + intercept)
             d = point_distance(point_a, point_b)
-            delta = 0.1
+            delta = setting.delta
             num_points = int(d / delta)
 
             t = np.linspace(0, 1, num_points)
@@ -73,10 +74,24 @@ if __name__ == '__main__':
             # z_remain = z[mask]
 
             w = 2 * np.pi * (t_remain - 0.5)
-            c = finufft.nufft1d1(w, z.astype(np.complex128), len(t_remain), eps=1e-30, isign=1)
+            # c = np.fft.fft(z)
+            # z_rec = np.fft.ifft(c)
+            sp_size = 32
+            upsampfac=3
+            spread_kerevalmeth=0
 
-            z_rec = finufft.nufft1d2(w, c, eps=1e-30, isign=-1) / len(t_remain)
-            print(f"Diff {setting.delta}", np.linalg.norm(z_rec - z))
+            c = finufft.nufft1d1(w, z.astype(np.complex128), len(t_remain), eps=1e-14, isign=1,
+                                 upsampfac=upsampfac,
+                                 spread_kerevalmeth=spread_kerevalmeth,
+                                 spread_max_sp_size=sp_size
+                                 )
+            z_rec = finufft.nufft1d2(w, c, eps=1e-14, isign=-1,
+                                     upsampfac=upsampfac,
+                                     spread_kerevalmeth=spread_kerevalmeth,
+                                     spread_max_sp_size=sp_size
+                                     ) / len(t_remain)
+            print(f"RMSE {setting.delta}: ", np.linalg.norm((z_rec - z)) / np.sqrt(len(z)))
+            print(f"Mean error {setting.delta}: ", np.mean(np.abs(z_rec - z)))
 
             ax.scatter(x, y, z_rec.real, color='blue', alpha=0.5)
             ax.set_xlabel('X')
@@ -84,4 +99,4 @@ if __name__ == '__main__':
             ax.set_zlabel('Z')
             ax.set_title('MLS on set I')
 
-        # plt.show(block=True)
+        plt.show(block=True)
